01ce04ae01243a4d5c683a1211a04067dccdba91
Zaksofon <81183855+Zaksofon@users.noreply.github.com>
2021-12-05 17:23:04 +0200

    Add files via upload

---
+++ Advanced Exercises/Functional Programming/Exercises/07. Predicate for Names/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7.Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+                .Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Predicate<string> filter = name => name.Length <= n;
+
+            Console.WriteLine(string.Join("\n", names.Where(name => filter(name))));
+        }
+    }
+}
---
+++ Advanced Exercises/Functional Programming/Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new[] {", "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
