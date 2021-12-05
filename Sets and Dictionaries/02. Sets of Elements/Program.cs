51ca8394b9b5873d74f8f2195b95882eae461cff
Zaksofon <81183855+Zaksofon@users.noreply.github.com>
2021-12-05 17:16:11 +0200

    Add files via upload

---
+++ Sets and Dictionaries/02. Sets of Elements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lengths = Console.ReadLine()
+                .Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            HashSet<int> firstSet = new HashSet<int>(lengths[0]);
+            HashSet<int> secondSet = new HashSet<int>(lengths[1]);
+
+            HashSet<int> result = new HashSet<int>();
+
+            for (int i = 0; i < lengths[0]; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                firstSet.Add(numbers);
+            }
+
+            for (int j = 0; j < lengths[1]; j++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                secondSet.Add(numbers);
+            }
+
+            foreach (var number in firstSet)
+            {
+                if (secondSet.Contains(number))
+                {
+                    result.Add(number);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
---
+++ Sets and Dictionaries/06. Wordrobe/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(new []{" -> "}, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string color = input[0];
+
+                string[] item = input[1]
+                    .Split(new[] {","}, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                foreach (var currentCloth in item)
+                {
+                    if (!wardrobe.ContainsKey(color))
+                    {
+                        wardrobe.Add(color, new Dictionary<string, int>());
+
+                        if (!color.Contains(currentCloth))
+                        {
+                            wardrobe[color].Add(currentCloth, 1);
+                            continue;
+                        }
+
+                        wardrobe[color][currentCloth]++;
+                    }
+
+                    if (wardrobe[color].ContainsKey(currentCloth))
+                    {
+                        wardrobe[color][currentCloth]++;
+                        continue;
+                    }
+                    wardrobe[color].Add(currentCloth, 1);
+                }
+            }
+
+            string[] lookingFor = Console.ReadLine()
+                .Split(new []{" "}, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string lookingForColor = lookingFor[0];
+            string lookingForItem = lookingFor[1];
+
+                foreach (var color in wardrobe)
+                {
+                    Console.WriteLine($"{color.Key} clothes:");
+
+                    foreach (var item in color.Value)
+                    {
+                        if (color.Key == lookingForColor && item.Key == lookingForItem)
+                        {
+                            Console.WriteLine($"* {item.Key} - {item.Value} (found!)");
+                            continue;
+                        }
+                        Console.WriteLine($"* {item.Key} - {item.Value}");
+                    }
+                }
+        }
+    }
+}
